6851fc1a8d8586caaac35927d0e43fa66074d488
Harold-M-Suquillo <haroldsuquillo@gmail.com>
2022-06-19 17:44:13 -0400

    App can aquire Weather and Places data

--- index.js
+++ index.js
@@ -2,14 +2,34 @@
 import {SearchModel, WeatherModel} from './js/model.js';
 import {View} from './js/view.js';
 class Controller{
-    constructor(){
-        console.log("Constructor Called Controller");
+    constructor(Search, Weather){
         this.search = new SearchModel();
         this.weather = new WeatherModel();
         this.view = new View();
+
+
+        document.getElementById("submit-btn").addEventListener('click', this.handleSearchInput.bind(this));
     }
 
+
+    handleSearchInput(e){
+        e.preventDefault();
+        this.getWeather();       
+    }
+    
+    getWeather(){
+        console.log("Entered function")
+        this.weather.fetchdata()
+       // this.weather.fetchdata();
+    }
+
+
+
 }
+
+// SET YOUR API KEYS
+const PLACES_API_KEY = null;
+const WEATHER_API_KEY = null;
 
 // Begin the application
 let a = new Controller();
--- js/model.js
+++ js/model.js
@@ -1,36 +1,40 @@
 
+
 export class SearchModel{
     static autocomplete = 0;
-
+    static place = null;
     constructor(){
         this.initAutocomplete();    // Initalize the search
-
     }
-        // Google Autocomplete
-    autocomplete = 0;
     initAutocomplete() {
         SearchModel.autocomplete = new google.maps.places.Autocomplete(
             document.getElementById('autocomplete'), 
             {
                 //types: ['geocode'],
                 //componentRestrictions: {'country': ['AU']},
-                fields: ['place_id', 'geometry', 'formatted_address','name']
+                fields: ['place_id', 'geometry', 'formatted_address','name',]
             });
-            //console.log(autocomplete); // ---------------------------
-            //SearchModel.autocomplete.addListener('place_changed', this.onPlaceChanged);
+            SearchModel.autocomplete.addListener('place_changed', this.onPlaceChanged);
     }
 
     // Handles Option 
     onPlaceChanged(){
-        //console.log(autocomplete); // ---------------------------
-        let place =SearchModel.autocomplete.getPlace();
-        console.log(place);         // ---------------------------
+        let place = SearchModel.autocomplete.getPlace();
         if (!place.geometry){
             // User did not select a prediction; reset the input field
             document.getElementById('autocomplete').placeholder = 'Enter a place';
+            SearchModel.place = null;
         }
         else{
-            //document.getElementById('details').innerHTML = place.geometry.location;
+            // find set the latitude and longitude
+            //console.log(place.geometry.location);
+            console.log(place);
+            SearchModel.place = {
+                formatted_address: place.formatted_address,
+                latitude: place.geometry.location.lat(),
+                longitude: place.geometry.location.lng()
+                }
+                
         }
 }
 
@@ -41,7 +45,26 @@ export class SearchModel{
 // Handle call to Weather API
 export class WeatherModel{
     constructor(){
-        this.a = 0;
+        this.data = null;
     }
+    async fetchdata(){
+        try{
+            const response = await fetch("http://api.weatherapi.com/v1/forecast.json?key=5acdfa352226498c98955739221706&q=41.394817,-73.4540111&days=3");
+            if (response.ok){
+                console.log("Response Gotten");
+                const jsonResponse = await response.json();
+                console.log(jsonResponse);
+
+            }
+        }
+        catch(error){
+            console.log("Failed to GET");
+            paragraph.innerHTML = error;
+        }
+
+
+    }
+
+
 
 }
