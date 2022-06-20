462d1a224bf6643f66023f9f31830dc4284c6351
Harold-M-Suquillo <haroldsuquillo@gmail.com>
2022-06-20 18:41:47 -0400

    Finished UI

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 // Controller UI
 import {SearchModel, WeatherModel} from './js/model.js';
 import {View} from './js/view.js';
-class Controller{
-    constructor(Search, Weather){
-        this.search = new SearchModel();
-        this.weather = new WeatherModel();
-        this.view = new View();
-        this.data = null;
-        document.getElementById("submit-btn").addEventListener('click', this.handleSearchInput.bind(this));
-    }
-    
-    handleSearchInput(e){
-        e.preventDefault();
-        this.weather.fetchdata(SearchModel.place).then(this.handleUI);
-    }
-    handleUI(data){
-        console.log(data);
-    }
-
-
-}
 
 // SET YOUR API KEYS
 const PLACES_API_KEY = null;
 const WEATHER_API_KEY = null;
+
+class Controller{
+    constructor(){
+        this.view = new View();
+        this.search = new SearchModel();
+        this.weather = new WeatherModel();
+        document.getElementById("submit-btn").addEventListener('click', this.handleRequest.bind(this));
+    }
+    _weatherRequest(){
+        return this.weather.fetchdata(SearchModel.place);
+    }
+    _handleUI(data){
+        //console.log(this);
+        this.view.CreateAndDisplay(data);
+    }
+
+    handleRequest(e){
+        e.preventDefault();
+        let promise = this._weatherRequest();
+        promise.then(this._handleUI.bind(this));
+        
+    }
+
+
+
+}
+
 
 // Begin the application
 let a = new Controller();
--- js/view.js
+++ js/view.js
@@ -1,3 +1,4 @@
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 export class View{
     constructor(){
         setInterval(this.clock, 1000);
@@ -11,6 +12,89 @@ export class View{
         time.textContent = `${hours}:${minutes}:${seconds}`;
     }
 
+    // Creates all the elements and add to the DOM
+    CreateAndDisplay(e){
+        let container = document.getElementById("weather-container")
+        // Remove old data
+        let child = container.lastElementChild; 
+        while (child) {
+            container.removeChild(child);
+            child = container.lastElementChild;
+        }
+        // Append all the new elements to the DOM
+        for (let i = 0; i < 3; i++){
+            container.append(this._CreateElement(i, e.forecast.forecastday[i]))
+        }
+    }
+
+    // Acquire the date
+    _AcquireDate(i){
+        var day = new Date();
+        var nextDay = new Date(day);
+        nextDay.setDate(day.getDate() + i);
+        return days[nextDay.getDay()] + " " + nextDay.getDate();
+    }
+
+    _AquireIcon(data){
+        let icon = data.day.condition.icon.split("/");
+        icon = icon[icon.length - 1]
+        return "./img/day/"+icon;
+    }
+
+    _AcquireDayInfo(data){
+        // Forecast/ Max temp/ Min temp
+        return {
+            maxTemp: data.day.maxtemp_f + " °F",
+            minTemp: data.day.mintemp_f + " °F",
+            forecast: data.day.condition.text
+        };
+
+    }
+
+    // Create sinle card element
+    _CreateElement(id, data){
+
+        let NewElement = Object.assign(document.createElement('div'),{classList:"col-lg-3 col-sm-6 py-3"});
+        let temp = Object.assign(document.createElement('div'),{classList:"bg-container"});
+        NewElement.append(temp);
+        let temp2 = Object.assign(document.createElement('div'),{classList:"text-center card"});
+        temp.append(temp2);
+
+        let date = this._AcquireDate(id);   // Get the date
+        temp2.append(Object.assign(document.createElement('div'),{classList:"card-header h3 m-0", textContent:date}));
+
+        let temp3 = document.createElement("div");  // Data Container
+        temp3.classList.add("card-body");
+        temp2.append(temp3);        
+        temp3.append(Object.assign(document.createElement('img'),{src:this._AquireIcon(data)}));    // Assign the icon
+
+    
+        // Add the forecast data 
+        let forecast = this._AcquireDayInfo(data);
+        temp3.append(Object.assign(document.createElement('p'),{classList:"card-text mt-3 m-0", textContent:`Forecast: ${forecast.forecast}`}));
+        temp3.append(Object.assign(document.createElement('p'),{classList:"card-text m-1", textContent:`Max Temp: ${forecast.maxTemp}`}));
+        temp3.append(Object.assign(document.createElement('p'),{classList:"card-text m-1", textContent:`Min Temp: ${forecast.minTemp}`}));
+
+
+
+        return NewElement;
+
+
+
+
+        // temp2 is <div class="card text-center">
+        // Date 
+
+
+        // Image
+
+
+
+        // Forecast/min and max temp
+
+
+
+    }
 
 
 
